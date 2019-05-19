9380fba78153b10efb237e49a5df2c69fa23269f
dominguezmaria <mariacristinavfdominguez@gmail.com>
2019-05-19 13:02:02 +0100

    Add step numbers to exercise 2

--- exercise_2/sorting_words.py
+++ exercise_2/sorting_words.py
@@ -1,14 +1,18 @@
+# Step 1
 def select_strings(l):
     return [i for i in l if isinstance(i, str)]
 
 
+# Step 2
 def clean_string(x):
     return x.strip().lower()
 
 
+# Step 3
 def sort_string_list(l):
     return sorted(l)
 
 
+# Step 4
 def sort_this(l):
     return sort_string_list([clean_string(i) for i in select_strings(l)])
