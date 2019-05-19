1167096eef1cd2521258d2d469bba1619b62a2d0
dominguezmaria <mariacristinavfdominguez@gmail.com>
2019-05-19 13:01:25 +0100

    Add step numbers to exercise 1

--- exercise_1/orthogonal_arrays.py
+++ exercise_1/orthogonal_arrays.py
@@ -1,3 +1,4 @@
+# Step 1
 def validate_tuples(a, b):
     if (not isinstance(a, tuple)) or (not isinstance(b, tuple)):
         raise ValueError("The input arguments must be tuples.")
@@ -5,6 +6,7 @@ def validate_tuples(a, b):
     return True
 
 
+# Step 2
 def validate_dimensions(a, b):
     if (len(a) < 2) or (len(b) < 2):
         raise ValueError("Input arrays must have at least two dimensions.")
@@ -14,10 +16,12 @@ def validate_dimensions(a, b):
     return [a[:min_dims], b[:min_dims]]
 
 
+# Step 3
 def inner_product(a, b):
     return sum([x_i * y_i for x_i, y_i in zip(a, b)])
 
 
+# Step 4
 def are_orthogonal(x, y):
     validate_tuples(x, y)
     x, y = validate_dimensions(x, y)
