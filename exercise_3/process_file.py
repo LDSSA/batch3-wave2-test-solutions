3e51d969c1a043df6dce961f1cb46231e8e0df82
dominguezmaria <mariacristinavfdominguez@gmail.com>
2019-05-19 13:00:50 +0100

    Improve exercise 3

--- exercise_3/process_file.py
+++ exercise_3/process_file.py
@@ -1,7 +1,6 @@
 def process_file(fp, separator):
     total = 0
     for line in fp:
-        print(line)
         line_split = line.split(separator)
         line_sum = sum([int(i) for i in line_split[1:]])
         if line_split[0] == '+':
--- exercise_3/test_process_file.py
+++ exercise_3/test_process_file.py
@@ -2,57 +2,41 @@ import io
 from process_file import process_file
 
 
-in_out_ok = {
-    (("+", 3, 4, 10), ("+", 1)): 18,
-    (("+", 3, 4, 5), ("-", 2, 6)): 4,
-    (("+", 1, 1), ("-", 5)): -3,
-    (("-", 1, 1, 3),): -5,
-    (("-", 11, 4), ('+', 15)): 0,
-    (("-", 11, 4), ('+', 15), ('=', 10)): 0,
-}
+def mock_fp(test_input, separator):
+    lines_list = [
+        separator.join(str(item) for item in raw_line)
+        for raw_line in test_input
+    ]
+    lines = '\n'.join(lines_list)
 
-
-def mock_fp(input_, separator):
-    lines = '\n'.join([separator.join(str(char) for char in raw_line) for raw_line in input_])
     return io.StringIO(lines)
 
 
-def process_file_(fp, separator):
-    result = 0
-    lines = [line.split(separator) for line in fp.readlines()]
-    print(lines)
-    for line in lines:
-        line_sum = sum(int(c) for c in line[1:])
-        if line[0] == '+':
-            result += line_sum
-        if line[0] == '-':
-            result -= line_sum
-        # anything else is ignored
-    return result
+def get_error_msg(test_input, separator, expected_output, computed):
+    return (
+        f"data used to generate file: {test_input}\n"
+        f'with separator: "{separator}"\n'
+        'ERROR!'
+        f"expected: {expected_output}\n"
+        f"got: {computed}"
+    )
 
 
-def err_msg(input_, separator, outpout_, computed):
-    return f"""
+def test_process_file():
+    test_cases = {
+        (("+", 3, 4, 10), ("+", 1)): 18,
+        (("+", 3, 4, 5), ("-", 2, 6)): 4,
+        (("+", 1, 1), ("-", 5)): -3,
+        (("-", 1, 1, 3),): -5,
+        (("-", 11, 4), ('+', 15)): 0,
+        (("-", 11, 4), ('+', 15), ('=', 10)): 0,
+    }
+    separators = [' ', ',', '::']
 
-        data used to generate file: {input_}
+    for test_input, expected_output in test_cases.items():
+        for sep in separators:
+            fp = mock_fp(test_input, sep)
+            answer = process_file(fp, sep)
 
-        with separator: "{separator}"
-
-        ERROR!
-
-        expected: {outpout_}
-
-        got: {computed}
-
-        """
-
-
-def test_process_file(in_out):
-    for i, o in in_out.items():
-        for separator in [' ', ',', '::']:
-            fp = mock_fp(i, separator)
-            computed = process_file(fp, separator)
-            assert computed == o, err_msg(i, separator, o, computed)
-
-
-test_process_file(in_out_ok)
+            error_msg = get_error_msg(test_input, sep, expected_output, answer)
+            assert answer == expected_output, error_msg
